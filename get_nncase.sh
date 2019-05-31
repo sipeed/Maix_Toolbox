c57594f93c2a8b4d596fc26577826faa0d01f523
Caize Wu <zepanwucai@gmail.com>
2019-06-01 01:11:38 +0800

    Update get_nncase.sh

--- get_nncase.sh
+++ get_nncase.sh
@@ -4,7 +4,7 @@ mkdir -p workspace
 mkdir -p images
 mkdir -p log
 cd ncc
-wget https://github.com/kendryte/nncase/releases/download/v0.1.0-rc2/ncc-linux-x86_64.tar.gz
-tar xzvf ncc-linux-x86_64.tar.gz
+wget https://github.com/kendryte/nncase/releases/download/v0.1.0-rc5/ncc-linux-x86_64.tar.xz
+tar -Jxf ncc-linux-x86_64.tar.gz
 rm ncc-linux-x86_64.tar.gz
 echo "download nncase ok!"
